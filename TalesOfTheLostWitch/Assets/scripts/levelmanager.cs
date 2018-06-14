375971744ed1755c0d1bc5a86c8d5bd0d2680bae
benji664 <40214773+benji664@users.noreply.github.com>
2018-06-14 14:36:41 -0600

    added button system and a couple of scenes

---
+++ TalesOfTheLostWitch/Assets/scripts/levelmanager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class levelmanager : MonoBehaviour {
+	public void loadlevel (string name){
+		Debug.Log ("level load requested for:" +name);
+		SceneManager.LoadScene(name);
+	}
+	public void QuitRequest (){
+		Debug.Log ("i wanna quit!");
+		Application.Quit();
+	}
+}
