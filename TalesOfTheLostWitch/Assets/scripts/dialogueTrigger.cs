252e4b4608bd919d76457f860c6e053ed53c2b95
benji664 <40214773+benji664@users.noreply.github.com>
2018-06-16 19:31:56 -0600

    added simple dialogue system

---
+++ TalesOfTheLostWitch/Assets/scripts/dialogueTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueTrigger : MonoBehaviour {
+
+	public Dialogue dialogue;
+
+	public void TriggerDialogue (){
+
+		FindObjectOfType<dialogueManager>().StartDialogue(dialogue);
+	}
+}
